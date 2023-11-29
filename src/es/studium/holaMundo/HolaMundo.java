61c61286675fe6f2e5225ceeeb8646d6cdd69ebc
denispena7 <penadenis007@gmail.com>
2023-11-29 13:43:18 +0100

    Segundo Commit

--- src/es/studium/holaMundo/HolaMundo.java
+++ src/es/studium/holaMundo/HolaMundo.java
@@ -4,6 +4,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
+		//Prueba para segundo commit
+		System.out.println("Subimos segundo commit a GitHub");
+		
 		System.out.println("Hola, mundo!");
 		System.out.print("Buenas!");
 		System.out.println();
