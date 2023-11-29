2f5b4e7a1d9c845486f7ac261a8a10356a99281c
denis_pena <penadenis007@gmail.com>
2023-11-29 12:54:35 +0100

    First commit

---
+++ src/es/studium/holaMundo/HolaMundo.java
@@ -0,0 +1,15 @@
+package es.studium.holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola, mundo!");
+		System.out.print("Buenas!");
+		System.out.println();
+		System.out.println();
+		System.out.println("Buenos dias");
+
+	}
+
+}
